984556bed67082f871d7364e7a27e53a5e0b4472
kgaidi <aydi.biznes@mail.ru>
2020-02-08 19:03:16 -0600

    new first commit

---
+++ src/main/java/io/agileint/newppmtool/NewppmtoolApplication.java
@@ -0,0 +1,13 @@
+package io.agileint.newppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewppmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewppmtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agileint/newppmtool/NewppmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileint.newppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewppmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
